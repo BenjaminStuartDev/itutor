05eb3291b7eaf58296717c0fee16dbaf558aedf3
Benjamin stuart <Benjamin.stuart001@outlook.com>
2022-05-11 16:26:03 +1000

    Attempt 1 at fixing migration bug

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,7 +1,5 @@
 require "active_support/core_ext/integer/time"
 
-
-
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -44,7 +42,6 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.perform_caching = false
-
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
