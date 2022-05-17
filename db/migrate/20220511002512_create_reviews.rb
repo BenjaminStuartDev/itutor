58af69f55e7119088bdba49776bb5f94e43acc77
Benjamin stuart <Benjamin.stuart001@outlook.com>
2022-05-17 10:10:27 +1000

    fixed reviews migration

--- db/migrate/20220511002512_create_reviews.rb
+++ db/migrate/20220511002512_create_reviews.rb
@@ -2,7 +2,7 @@ class CreateReviews < ActiveRecord::Migration[7.0]
   def change
     create_table :reviews do |t|
       t.references :tutor, null: false, index: true, foreign_key: { to_table: :users }
-      t.references :student, null: false, index: true, foreign_key: { to_table: :users }
+      t.references :student, null: true, index: true, foreign_key: { to_table: :users }
       t.text :content
       t.integer :rating
 
